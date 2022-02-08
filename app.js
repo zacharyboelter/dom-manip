ac48011debee54ac3445317391b8a62aa4478e33
Zachary Boelter <zboelter@gmail.com>
2022-02-08 14:10:48 -0600

    buttons by id

--- app.js
+++ app.js
@@ -1,39 +1,63 @@
 
-const container = document.querySelector('#container');
+// const container = document.querySelector('#container');
 
-const content = document.createElement('div');
+// const content = document.createElement('div');
 
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+// content.classList.add('content');
+// content.textContent = 'This is the glorious text-content!';
 
-container.appendChild(content);
+// container.appendChild(content);
 
-// create p that says hey im red
-const p = document.createElement('p');
-p.textContent = "Hey, I'm red!"
-p.style.color = 'red';
-document.getElementById('container').appendChild(p);
+// // create p that says hey im red
+// const p = document.createElement('p');
+// p.textContent = "Hey, I'm red!"
+// p.style.color = 'red';
+// document.getElementById('container').appendChild(p);
 
-// create h3 says hey im blue
-const h3 = document.createElement('h3');
-h3.textContent = 'I\'m a blue h3!';
-h3.style.color = 'blue';
-document.getElementById('container').appendChild(h3);
+// // create h3 says hey im blue
+// const h3 = document.createElement('h3');
+// h3.textContent = 'I\'m a blue h3!';
+// h3.style.color = 'blue';
+// document.getElementById('container').appendChild(h3);
 
-const pinkDiv = document.createElement('div');
-pinkDiv.style.border = 'thick solid black';
-pinkDiv.style.backgroundColor = 'pink';
-// document.getElementById('container').appendChild(pinkDiv);
+// const pinkDiv = document.createElement('div');
+// pinkDiv.style.border = 'thick solid black';
+// pinkDiv.style.backgroundColor = 'pink';
+// // document.getElementById('container').appendChild(pinkDiv);
 
-const head1 = document.createElement('h3')
-head1.textContent = 'I\'m a div';
+// const head1 = document.createElement('h3')
+// head1.textContent = 'I\'m a div';
 
-pinkDiv.appendChild(head1);
+// pinkDiv.appendChild(head1);
 
-const head2 = document.createElement('p');
-head2.textContent = 'ME TOO!';
+// const head2 = document.createElement('p');
+// head2.textContent = 'ME TOO!';
 
-pinkDiv.appendChild(head2);
+// pinkDiv.appendChild(head2);
 
 
-document.body.insertBefore(pinkDiv, null);
+// document.body.insertBefore(pinkDiv, null);
+
+
+
+const btn = document.querySelector('#btn');
+// btn.addEventListener('click', () => {
+//     alert('Hello World');
+// })
+
+function alertFunction() {
+    alert("HOLLA HOLLA YOU DID IT!!");
+}
+
+// btn.addEventListener('click', alertFunction);
+btn.addEventListener('click', function (e) {
+    console.log(e.target);
+});
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+});
